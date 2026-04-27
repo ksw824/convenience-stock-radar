47814e5525b307b6602b9cf535e69cc9c0d56b66
swKim <tjsdnrla1234@gmail.com>
2026-04-27 16:44:15 +0900

    refactor(role): add role key and title

--- src/main/java/org/example/convradar/domain/member/entity/Role.java
+++ src/main/java/org/example/convradar/domain/member/entity/Role.java
@@ -1,7 +1,15 @@
 package org.example.convradar.domain.member.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
-    USER,   //일반 사용자
-    OWNER,  //점주
-    ADMIN   //관리자
+    USER("ROLE_USER", "일반 사용자"),
+    OWNER("ROLE_OWNER", "점주"),
+    ADMIN("ROLE_ADMIN", "관리자");
+
+    private final String key;
+    private final String title;
 }
